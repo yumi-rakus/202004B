dc179f6f656b36a0d6c67073e5b8db6a06856827
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-14 17:29:21 +0900

    CurryController.javaにsetUpItemForm()、showDetail()、cartIn()を追加

--- src/main/java/com/example/controller/CurryController.java
+++ src/main/java/com/example/controller/CurryController.java
@@ -1,7 +1,20 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Item;
+import com.example.domain.Topping;
+import com.example.form.ItemForm;
+import com.example.service.ItemService;
+import com.example.service.ToppingService;
 
 /**
  * カレーECサイトを操作するコントローラ.
@@ -13,5 +26,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("")
 public class CurryController {
+
+	@Autowired
+	private ItemService itemService;
+
+	@Autowired
+	private ToppingService toppingService;
+
+	@ModelAttribute
+	public ItemForm setUpItemForm() {
+		return new ItemForm();
+	}
+
+	/**
+	 * 商品情報詳細画面を出力する.
+	 * 
+	 * @param id    商品ID
+	 * @param model モデル
+	 * @return 商品情報詳細画面
+	 */
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model, ItemForm itemForm) {
+
+		Item item = itemService.showDetail(Integer.parseInt(id));
+		List<Topping> toppingList = toppingService.showToppingList();
+
+		model.addAttribute("item", item);
+		model.addAttribute("toppingList", toppingList);
+
+		itemForm.setSize("M");
+		itemForm.setQuantity(1);
+
+		return "item_detail";
+	}
+
+	@RequestMapping("/cart-in")
+	public String cartIn(@Validated ItemForm form, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return "item_detail";
+		}
+
+		return "cart-in-complete";
+	}
 
 }
