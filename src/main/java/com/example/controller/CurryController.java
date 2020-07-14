6628eeef8890fcc8e2b19d3fa4e85ce5405c5415
etokohei <kohei.eto@rakus-partners.co.jp>
2020-07-14 15:09:10 +0900

    ユーザ登録ができる

--- src/main/java/com/example/controller/CurryController.java
+++ src/main/java/com/example/controller/CurryController.java
@@ -1,7 +1,16 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.User;
+import com.example.form.UserForm;
+import com.example.service.UserService;
+
+import org.springframework.beans.BeanUtils;
 
 /**
  * カレーECサイトを操作するコントローラ.
@@ -13,5 +22,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("")
 public class CurryController {
+
+	@Autowired
+	private UserService userService;
+
+	@ModelAttribute
+	public UserForm setUpUserForm() {
+		return new UserForm();
+	}
+
+	// ログイン画面を表示
+	@RequestMapping("")
+	public String index() {
+		return "login";
+	}
+
+	// 従業員登録画面を表示
+	@RequestMapping("/indexRegister")
+	public String indexRegister() {
+		return "register_user";
+	}
+
+	// 従業員登録をする
+	@RequestMapping("/register")
+	public String register(UserForm userForm) {
+		User user = new User();
+		BeanUtils.copyProperties(userForm, user);
+		userService.insert(user);
+		return "redirect:/";
+	}
 
 }
