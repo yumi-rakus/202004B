1ea8a49698551a49857d7e60fad20e92cff7ec49
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-16 18:24:28 +0900

    OrderService.javaを修正

--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -93,6 +93,48 @@ public class OrderService {
 	}
 
 	/**
+	 * 注文履歴を検索する
+	 * 
+	 * @param userId
+	 * @return 注文履歴
+	 */
+	public List<Order> findOrderHistory(Integer userId) {
+		List<Order> orderList = orderRepository.findByUserIdAndNonStatus0(userId);
+
+		Map<Integer, List<OrderTopping>> orderToppingMap = new HashMap<>();
+		Map<Integer, OrderItem> orderItemMap = new HashMap<>();
+		Map<Integer, Order> orderMap = new HashMap<>();
+
+		for (Order order : orderList) {
+			orderToppingMap.put(order.getOrderItemList().get(0).getId(), new ArrayList<>());
+		}
+		for (Order order : orderList) {
+			List<OrderTopping> orderToppingList = orderToppingMap.get(order.getOrderItemList().get(0).getId());
+			orderToppingList.add(order.getOrderItemList().get(0).getOrderToppingList().get(0));
+		}
+		for (Order order : orderList) {
+			orderItemMap.put(order.getOrderItemList().get(0).getId(), order.getOrderItemList().get(0));
+		}
+		for (Order order : orderList) {
+			OrderItem orderItem = orderItemMap.get(order.getOrderItemList().get(0).getId());
+			List<OrderTopping> orderToppingList = orderToppingMap.get(order.getOrderItemList().get(0).getId());
+			orderItem.setOrderToppingList(orderToppingList);
+			orderItemMap.put(order.getOrderItemList().get(0).getId(), orderItem);
+		}
+
+		List<OrderItem> orderItemList = new ArrayList<>(orderItemMap.values());
+
+		for (Order order : orderList) {
+			order.setOrderItemList(orderItemList);
+			orderMap.put(order.getId(), order);
+		}
+
+		List<Order> distinctOrderList = new ArrayList<>(orderMap.values());
+
+		return distinctOrderList;
+	}
+
+	/**
 	 * status = 0 (未注文)のレコードが存在するかどうかを返す.
 	 * 
 	 * @param userId ユーザID
