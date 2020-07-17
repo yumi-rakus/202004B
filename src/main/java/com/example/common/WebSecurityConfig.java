20bfc71fda9c4a315a0f7c9cbf730609ca33b988
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-07-17 18:09:33 +0900

    URLのリストをアルファベット順に並べ替え

--- src/main/java/com/example/common/WebSecurityConfig.java
+++ src/main/java/com/example/common/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-			.antMatchers("/toLogin", "/insertUser", "/", "/search","/showDetail", "/order", "/indexRegister", "/register", "/showCartList", "/deleteOrderItem", "/cartIn", "/cartInComplete", "/ajax/**").permitAll()
+			.antMatchers("/", "/ajax/**", "/cartIn", "/cartInComplete", "/deleteOrderItem", "/indexRegister", "/insertUser", "/order", "/register", "/showCartList", "/search", "/showDetail", "/toLogin").permitAll()
 			.anyRequest().authenticated()
 			.and()
 		.formLogin()
