b12dc37dfaff23ba8053ee482b9471010f643641
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-20 12:12:05 +0900

    OrderToppingService.javaにgetToppingIdListByOrderItemId()を追加

--- src/main/java/com/example/service/OrderToppingService.java
+++ src/main/java/com/example/service/OrderToppingService.java
@@ -5,6 +5,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.domain.OrderTopping;
 import com.example.repository.OrderToppingRepository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +32,18 @@ public class OrderToppingService {
 	 */
 	public void insertOrderTopping(OrderTopping orderTopping) {
 		orderToppingRepository.insertOrderTopping(orderTopping);
+	}
+
+	/**
+	 * 注文トッピングIDリストを取得する.
+	 * 
+	 * @param orderItemId 注文商品ID
+	 * @return 注文トッピングIDリスト
+	 * 
+	 * @author yumi takahashi
+	 */
+	public List<Integer> getToppingIdListByOrderItemId(Integer orderItemId) {
+		return orderToppingRepository.getToppingIdListByOrderItemId(orderItemId);
 	}
 
 }
