9f831d1248321d3f55b93ee8f4fb1a79a29a6b07
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-21 10:45:36 +0900

    cart_list.jsを作成（数量変更時に小計、消費税、合計金額を更新するJS）

---
+++ src/main/resources/static/js/cart_list.js
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+$(function () {
+	
+			$('#deleteAll').on('click', function () {
+				if (window.confirm('ショッピングカートの中身を全件削除してよろしいですか？')) {
+					return true;
+				} else {
+					return false;
+				}
+			});
+			
+			$('.quantity').on('change', function(){
+				
+				setCsrfTokenToAjaxHeader();
+				
+				var quantity = $(this).val();
+				var itemId = $(this).siblings('.itemId').val();
+				var size = $(this).siblings('.price').text().trim();
+				
+				var price = parseInt($(this).siblings('.itemPrice').text().replace(',', '').replace('円', ''));
+		
+				var toppingPriceList = $(this).parent().next().children().children().children().children('td.text-left').children('.toppingPrice').text().split('円');				
+				toppingPriceList.pop();
+				
+				var toppingPrice = 0;
+				for(var topPrice of toppingPriceList){
+					toppingPrice = toppingPrice + parseInt(topPrice);
+				}
+				
+				var subTotal = (price + toppingPrice) * quantity;
+				$(this).parent().next().next().children().children('.subTotal').text(subTotal.toLocaleString());
+				
+				update(quantity, itemId, size, subTotal);
+			});
+		});
+		
+		function update(quantity, itemId, size, subTotal){
+			
+			$.ajax({
+				type: "POST",
+				url: "/ajax/totalPrice",
+				data: {
+					"newQuantity": quantity,
+					"itemId": itemId,
+					"size": size
+				},
+				dataType: "json"
+			}).done(function(updateMap){
+				
+				var newTotalPrice = updateMap["totalPrice"];
+				var newTax = updateMap["tax"];
+			
+				$('.tax').text('消費税：' + newTax.toLocaleString() + '円');
+				$('.totalPrice').text('ご注文金額合計：' + newTotalPrice.toLocaleString() + '円');
+				
+			}).fail(function(XMLHttpRequest,textStatus,errorThrown){
+		    	alert("リクエストに失敗"+textStatus+":\n"+errorThrown)
+		    })
+			
+		}
+		
+		function setCsrfTokenToAjaxHeader() {
+	        var token = $("meta[name='_csrf']").attr("content");
+	        var header = $("meta[name='_csrf_header']").attr("content");
+	        $(document).ajaxSend(function(e, xhr, options) {
+	            xhr.setRequestHeader(header, token);
+	        });
+	    }
