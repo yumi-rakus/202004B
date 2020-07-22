f962596542bd2aaf05b67590150c8f181ba5800d
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-07-22 12:28:11 +0900

    テスト用のクラスを作成

---
+++ src/test/java/com/example/repository/FavoriteRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.example.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FavoriteRepositoryTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
