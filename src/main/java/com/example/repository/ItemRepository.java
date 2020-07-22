d24561fabce8adc287095c3ec19339a9ab71975c
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-22 16:24:07 +0900

    ItemRepository.javaにfindAll()、updateDeleteFlag()を追加、もともとあったfindAll()をfindAllNonDeleted()に改名（商品リストを取得する各メソッドのSQL文のWHERE句にdeleted = falseを追記）

--- src/main/java/com/example/repository/ItemRepository.java
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -10,7 +10,6 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Item;
-import com.example.domain.User;
 
 /**
  * itemsテーブルを操作するリポジトリ.
@@ -47,6 +46,8 @@ public class ItemRepository {
 	 * 
 	 * @param id 商品ID
 	 * @return 商品情報
+	 * 
+	 * @author yumi takahashi
 	 */
 	public Item load(Integer id) {
 
@@ -60,15 +61,15 @@ public class ItemRepository {
 	}
 
 	/**
-	 * 全商品情報を取得する.
+	 * (削除フラグのたっていない)全商品情報を取得する.
 	 * 
 	 * @return 商品情報一覧
 	 * 
 	 * @author kohei eto
 	 */
-	public List<Item> findAll() {
+	public List<Item> findAllNonDeleted() {
 
-		String sql = "select id, name, description, price_m, price_l, image_path, deleted from items order by price_m";
+		String sql = "select id, name, description, price_m, price_l, image_path, deleted from items where deleted = false order by price_m";
 
 		List<Item> itemlist = template.query(sql, ITEM_ROW_MAPPER);
 
@@ -86,7 +87,7 @@ public class ItemRepository {
 
 	// あいまい検索で価格安い順に取得
 	public List<Item> findByItemName(String name) {
-		String sql = "select * from items where name like :name order by price_m";
+		String sql = "select * from items where name like :name and deleted = false order by price_m";
 
 		SqlParameterSource param = new MapSqlParameterSource().addValue("name", "%" + name + "%");
 
@@ -98,7 +99,7 @@ public class ItemRepository {
 
 	// あいまい検索で価格高い順に取得
 	public List<Item> findByItemName2(String name) {
-		String sql = "select * from items where name like :name order by price_m desc";
+		String sql = "select * from items where name like :name and deleted = false  order by price_m desc";
 
 		SqlParameterSource param = new MapSqlParameterSource().addValue("name", "%" + name + "%");
 
@@ -109,7 +110,7 @@ public class ItemRepository {
 
 	// あいまい検索でid順で取得
 	public List<Item> findByItemName3(String name) {
-		String sql = "select * from items where name like :name order by id";
+		String sql = "select * from items where name like :name and deleted = false  order by id";
 
 		SqlParameterSource param = new MapSqlParameterSource().addValue("name", "%" + name + "%");
 
@@ -121,7 +122,7 @@ public class ItemRepository {
 	// 全商品情報を価格安い順で取得
 	public List<Item> findAllByPrice() {
 
-		String sql = "select id, name, description, price_m, price_l, image_path, deleted from items order by price_m";
+		String sql = "select id, name, description, price_m, price_l, image_path, deleted from items where deleted = false order by price_m";
 
 		List<Item> itemlist = template.query(sql, ITEM_ROW_MAPPER);
 
@@ -131,7 +132,7 @@ public class ItemRepository {
 	// 全商品情報を価格高い順で取得
 	public List<Item> findAllByPrice2() {
 
-		String sql = "select id, name, description, price_m, price_l, image_path, deleted from items order by price_m desc";
+		String sql = "select id, name, description, price_m, price_l, image_path, deleted from items where deleted = false order by price_m desc";
 
 		List<Item> itemlist = template.query(sql, ITEM_ROW_MAPPER);
 
@@ -141,11 +142,43 @@ public class ItemRepository {
 	// 全商品情報をid順で取得
 	public List<Item> findAllByPrice3() {
 
-		String sql = "select id, name, description, price_m, price_l, image_path, deleted from items order by id";
+		String sql = "select id, name, description, price_m, price_l, image_path, deleted from items where deleted = false order by id";
 
 		List<Item> itemlist = template.query(sql, ITEM_ROW_MAPPER);
 
 		return itemlist;
 	}
 
+	/**
+	 * 全商品情報を取得する.
+	 * 
+	 * @return 商品情報一覧
+	 * 
+	 * @author yumi takahashi
+	 */
+	public List<Item> findAll() {
+
+		String sql = "SELECT id, name, description, price_m, price_l, image_path, deleted FROM items ORDER BY id";
+
+		List<Item> itemList = template.query(sql, ITEM_ROW_MAPPER);
+
+		return itemList;
+	}
+
+	/**
+	 * 商品の削除フラグを更新する.
+	 * 
+	 * @param deleted 削除フラグ(true:商品一覧画面から除去、false:商品一覧画面に表示)
+	 * @param itemId  商品ID
+	 * 
+	 * @author yumi takahashi
+	 */
+	public void updateDeleteFlag(Boolean deleted, Integer itemId) {
+
+		String sql = "UPDATE items SET deleted = :deleted WHERE id = :itemId";
+
+		SqlParameterSource param = new MapSqlParameterSource().addValue("deleted", deleted).addValue("itemId", itemId);
+
+		template.update(sql, param);
+	}
 }
