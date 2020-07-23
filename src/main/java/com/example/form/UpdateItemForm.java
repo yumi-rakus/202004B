5cecd92dceda86c57b54058f8b7bb059344375af
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-23 20:35:35 +0900

    UpdateItemForm.javaを修正

--- src/main/java/com/example/form/UpdateItemForm.java
+++ src/main/java/com/example/form/UpdateItemForm.java
@@ -2,19 +2,29 @@ package com.example.form;
 
 import javax.validation.constraints.*;
 
+/**
+ * 商品情報を更新する際に使用するフォーム.
+ * 
+ * @author yumi takahashi
+ *
+ */
 public class UpdateItemForm {
 
+	/** 商品名 */
 	@NotBlank(message = "商品名を入力してください")
 	@Size(min = 0, max = 30, message = "商品名は30文字以内で入力してください")
 	private String name;
 
+	/** 商品説明 */
 	@NotBlank(message = "商品説明を入力してください")
 	@Size(min = 0, max = 140, message = "商品説明は140文字以内で入力してください")
 	private String description;
 
+	/** Mの価格 */
 	@NotNull(message = "Mサイズの価格を入力してください")
 	private Integer priceM;
 
+	/** Lの価格 */
 	@NotNull(message = "Lサイズの価格を入力してください")
 	private Integer priceL;
 
