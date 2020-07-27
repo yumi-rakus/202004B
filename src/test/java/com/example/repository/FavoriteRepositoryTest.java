423560f0d0badd2e03f9102942a08dc67fc7c251
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-27 16:18:43 +0900

    FavoriteRepositoryTest.javaを作成

--- src/test/java/com/example/repository/FavoriteRepositoryTest.java
+++ src/test/java/com/example/repository/FavoriteRepositoryTest.java
@@ -2,13 +2,33 @@ package com.example.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FavoriteRepositoryTest {
 
-  @Test
-  void test() {
-    fail("Not yet implemented");
-  }
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
 
 }
