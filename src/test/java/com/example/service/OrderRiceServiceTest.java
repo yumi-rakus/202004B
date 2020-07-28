88146c21a1e153e33626d767337b1b67e1bde60a
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-28 12:26:48 +0900

    OrderRiceServiceTest.java アノテーションを変更

--- src/test/java/com/example/service/OrderRiceServiceTest.java
+++ src/test/java/com/example/service/OrderRiceServiceTest.java
@@ -1,17 +1,17 @@
 package com.example.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.example.domain.OrderRice;
 
@@ -21,7 +21,7 @@ import com.example.domain.OrderRice;
  * @author yumi takahashi
  *
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class OrderRiceServiceTest {
 
