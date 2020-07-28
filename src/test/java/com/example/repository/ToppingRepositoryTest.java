0b4133b612c621d6431bf52a6f18e283edfbe1fb
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2020-07-28 12:24:26 +0900

    ToppingRepositoryTest.java アノテーションを変更

--- src/test/java/com/example/repository/ToppingRepositoryTest.java
+++ src/test/java/com/example/repository/ToppingRepositoryTest.java
@@ -1,18 +1,18 @@
 package com.example.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.example.domain.Topping;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ToppingRepositoryTest {
 
@@ -26,143 +26,143 @@ class ToppingRepositoryTest {
 
 		assertEquals(28, toppingList.size(), "件数が期待される結果と異なります。");
 
-		assertEquals(19, toppingList.get(0).getId(), "IDが期待される結果と異なります。");
-		assertEquals("あらびきスライスソｰセｰジ", toppingList.get(0).getName(), "名前が期待される結果と異なります。");
+		assertEquals(1, toppingList.get(0).getId(), "IDが期待される結果と異なります。");
+		assertEquals("オニオン", toppingList.get(0).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(0).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(0).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(6, toppingList.get(1).getId(), "IDが期待される結果と異なります。");
-		assertEquals("アンチョビ", toppingList.get(1).getName(), "名前が期待される結果と異なります。");
+		assertEquals(2, toppingList.get(1).getId(), "IDが期待される結果と異なります。");
+		assertEquals("ツナマヨ", toppingList.get(1).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(1).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(1).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(4, toppingList.get(2).getId(), "IDが期待される結果と異なります。");
-		assertEquals("イカ", toppingList.get(2).getName(), "名前が期待される結果と異なります。");
+		assertEquals(3, toppingList.get(2).getId(), "IDが期待される結果と異なります。");
+		assertEquals("イタリアントマト", toppingList.get(2).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(2).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(2).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(17, toppingList.get(3).getId(), "IDが期待される結果と異なります。");
-		assertEquals("イタリアンソーセージ", toppingList.get(3).getName(), "名前が期待される結果と異なります。");
+		assertEquals(4, toppingList.get(3).getId(), "IDが期待される結果と異なります。");
+		assertEquals("イカ", toppingList.get(3).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(3).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(3).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(3, toppingList.get(4).getId(), "IDが期待される結果と異なります。");
-		assertEquals("イタリアントマト", toppingList.get(4).getName(), "名前が期待される結果と異なります。");
+		assertEquals(5, toppingList.get(4).getId(), "IDが期待される結果と異なります。");
+		assertEquals("プルコギ", toppingList.get(4).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(4).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(4).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(7, toppingList.get(5).getId(), "IDが期待される結果と異なります。");
-		assertEquals("エビ", toppingList.get(5).getName(), "名前が期待される結果と異なります。");
+		assertEquals(6, toppingList.get(5).getId(), "IDが期待される結果と異なります。");
+		assertEquals("アンチョビ", toppingList.get(5).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(5).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(5).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(1, toppingList.get(6).getId(), "IDが期待される結果と異なります。");
-		assertEquals("オニオン", toppingList.get(6).getName(), "名前が期待される結果と異なります。");
+		assertEquals(7, toppingList.get(6).getId(), "IDが期待される結果と異なります。");
+		assertEquals("エビ", toppingList.get(6).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(6).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(6).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(18, toppingList.get(7).getId(), "IDが期待される結果と異なります。");
-		assertEquals("ガーリックスライス", toppingList.get(7).getName(), "名前が期待される結果と異なります。");
+		assertEquals(8, toppingList.get(7).getId(), "IDが期待される結果と異なります。");
+		assertEquals("コーン", toppingList.get(7).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(7).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(7).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(15, toppingList.get(8).getId(), "IDが期待される結果と異なります。");
-		assertEquals("カマンベールチーズ", toppingList.get(8).getName(), "名前が期待される結果と異なります。");
+		assertEquals(9, toppingList.get(8).getId(), "IDが期待される結果と異なります。");
+		assertEquals("ピーマン", toppingList.get(8).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(8).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(8).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(21, toppingList.get(9).getId(), "IDが期待される結果と異なります。");
-		assertEquals("グリーンアスパラ", toppingList.get(9).getName(), "名前が期待される結果と異なります。");
+		assertEquals(10, toppingList.get(9).getId(), "IDが期待される結果と異なります。");
+		assertEquals("フレッシュスライストマト", toppingList.get(9).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(9).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(9).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(8, toppingList.get(10).getId(), "IDが期待される結果と異なります。");
-		assertEquals("コーン", toppingList.get(10).getName(), "名前が期待される結果と異なります。");
+		assertEquals(11, toppingList.get(10).getId(), "IDが期待される結果と異なります。");
+		assertEquals("ベーコン", toppingList.get(10).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(10).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(10).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(28, toppingList.get(11).getId(), "IDが期待される結果と異なります。");
-		assertEquals("チーズ増量", toppingList.get(11).getName(), "名前が期待される結果と異なります。");
+		assertEquals(12, toppingList.get(11).getId(), "IDが期待される結果と異なります。");
+		assertEquals("ペパロニ･サラミ", toppingList.get(11).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(11).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(11).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(2, toppingList.get(12).getId(), "IDが期待される結果と異なります。");
-		assertEquals("ツナマヨ", toppingList.get(12).getName(), "名前が期待される結果と異なります。");
+		assertEquals(13, toppingList.get(12).getId(), "IDが期待される結果と異なります。");
+		assertEquals("熟成ベーコン", toppingList.get(12).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(12).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(12).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(23, toppingList.get(13).getId(), "IDが期待される結果と異なります。");
-		assertEquals("パイナップル", toppingList.get(13).getName(), "名前が期待される結果と異なります。");
+		assertEquals(14, toppingList.get(13).getId(), "IDが期待される結果と異なります。");
+		assertEquals("特製マヨソース", toppingList.get(13).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(13).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(13).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(24, toppingList.get(14).getId(), "IDが期待される結果と異なります。");
-		assertEquals("ハラペーニョ", toppingList.get(14).getName(), "名前が期待される結果と異なります。");
+		assertEquals(15, toppingList.get(14).getId(), "IDが期待される結果と異なります。");
+		assertEquals("カマンベールチーズ", toppingList.get(14).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(14).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(14).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(22, toppingList.get(15).getId(), "IDが期待される結果と異なります。");
-		assertEquals("パルメザンチーズ", toppingList.get(15).getName(), "名前が期待される結果と異なります。");
+		assertEquals(16, toppingList.get(15).getId(), "IDが期待される結果と異なります。");
+		assertEquals("フレッシュモッツァレラチーズ", toppingList.get(15).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(15).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(15).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(9, toppingList.get(16).getId(), "IDが期待される結果と異なります。");
-		assertEquals("ピーマン", toppingList.get(16).getName(), "名前が期待される結果と異なります。");
+		assertEquals(17, toppingList.get(16).getId(), "IDが期待される結果と異なります。");
+		assertEquals("イタリアンソーセージ", toppingList.get(16).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(16).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(16).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(27, toppingList.get(17).getId(), "IDが期待される結果と異なります。");
-		assertEquals("ブラックオリーブ", toppingList.get(17).getName(), "名前が期待される結果と異なります。");
+		assertEquals(18, toppingList.get(17).getId(), "IDが期待される結果と異なります。");
+		assertEquals("ガーリックスライス", toppingList.get(17).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(17).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(17).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(5, toppingList.get(18).getId(), "IDが期待される結果と異なります。");
-		assertEquals("プルコギ", toppingList.get(18).getName(), "名前が期待される結果と異なります。");
+		assertEquals(19, toppingList.get(18).getId(), "IDが期待される結果と異なります。");
+		assertEquals("あらびきスライスソｰセｰジ", toppingList.get(18).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(18).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(18).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(10, toppingList.get(19).getId(), "IDが期待される結果と異なります。");
-		assertEquals("フレッシュスライストマト", toppingList.get(19).getName(), "名前が期待される結果と異なります。");
+		assertEquals(20, toppingList.get(19).getId(), "IDが期待される結果と異なります。");
+		assertEquals("ブロッコリー", toppingList.get(19).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(19).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(19).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(16, toppingList.get(20).getId(), "IDが期待される結果と異なります。");
-		assertEquals("フレッシュモッツァレラチーズ", toppingList.get(20).getName(), "名前が期待される結果と異なります。");
+		assertEquals(21, toppingList.get(20).getId(), "IDが期待される結果と異なります。");
+		assertEquals("グリーンアスパラ", toppingList.get(20).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(20).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(20).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(20, toppingList.get(21).getId(), "IDが期待される結果と異なります。");
-		assertEquals("ブロッコリー", toppingList.get(21).getName(), "名前が期待される結果と異なります。");
+		assertEquals(22, toppingList.get(21).getId(), "IDが期待される結果と異なります。");
+		assertEquals("パルメザンチーズ", toppingList.get(21).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(21).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(21).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(11, toppingList.get(22).getId(), "IDが期待される結果と異なります。");
-		assertEquals("ベーコン", toppingList.get(22).getName(), "名前が期待される結果と異なります。");
+		assertEquals(23, toppingList.get(22).getId(), "IDが期待される結果と異なります。");
+		assertEquals("パイナップル", toppingList.get(22).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(22).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(22).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(12, toppingList.get(23).getId(), "IDが期待される結果と異なります。");
-		assertEquals("ペパロニ･サラミ", toppingList.get(23).getName(), "名前が期待される結果と異なります。");
+		assertEquals(24, toppingList.get(23).getId(), "IDが期待される結果と異なります。");
+		assertEquals("ハラペーニョ", toppingList.get(23).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(23).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(23).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(26, toppingList.get(24).getId(), "IDが期待される結果と異なります。");
-		assertEquals("ポテト", toppingList.get(24).getName(), "名前が期待される結果と異なります。");
+		assertEquals(25, toppingList.get(24).getId(), "IDが期待される結果と異なります。");
+		assertEquals("もち", toppingList.get(24).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(24).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(24).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(25, toppingList.get(25).getId(), "IDが期待される結果と異なります。");
-		assertEquals("もち", toppingList.get(25).getName(), "名前が期待される結果と異なります。");
+		assertEquals(26, toppingList.get(25).getId(), "IDが期待される結果と異なります。");
+		assertEquals("ポテト", toppingList.get(25).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(25).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(25).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(13, toppingList.get(26).getId(), "IDが期待される結果と異なります。");
-		assertEquals("熟成ベーコン", toppingList.get(26).getName(), "名前が期待される結果と異なります。");
+		assertEquals(27, toppingList.get(26).getId(), "IDが期待される結果と異なります。");
+		assertEquals("ブラックオリーブ", toppingList.get(26).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(26).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(26).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 
-		assertEquals(14, toppingList.get(27).getId(), "IDが期待される結果と異なります。");
-		assertEquals("特製マヨソース", toppingList.get(27).getName(), "名前が期待される結果と異なります。");
+		assertEquals(28, toppingList.get(27).getId(), "IDが期待される結果と異なります。");
+		assertEquals("チーズ増量", toppingList.get(27).getName(), "名前が期待される結果と異なります。");
 		assertEquals(200, toppingList.get(27).getPriceM(), "Mサイズ価格が期待される結果と異なります。");
 		assertEquals(300, toppingList.get(27).getPriceL(), "Lサイズ価格が期待される結果と異なります。");
 	}
